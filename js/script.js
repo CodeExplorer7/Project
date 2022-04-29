8099cccc26a46bbaff00017922befdf4dae57fc0
Viacheslav <codeexplorer7@gmail.com>
2022-04-29 16:02:36 +0300

    первый блин комом

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
 "use strict";
 
-let numberOfFilms = prompt ('Сколько фильмов Вы уже посмотрели?','');
+const numberOfFilms = +prompt ('Сколько фильмов Вы уже посмотрели?','');
 console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+ 
+    },
+    actors: {
+
+    },
+    genres: [],
+    privat: false
+};
+
+const a = prompt ('Один из последних просмотренных фильмов?',''),
+      b = prompt ('На сколько оцените его?',''),
+      c = prompt ('Один из последних просмотренных фильмов?',''),
+      d = prompt ('На сколько оцените его?','');
+
+personalMovieDB.movies [a] = b;
+personalMovieDB.movies [c] = d;
+
+console.log(personalMovieDB);
